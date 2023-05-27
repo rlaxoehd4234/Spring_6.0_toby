720544e13e1b0d81677617ef57aa6e66c50035fc
kimtaedong <rlaxoehd4234@naver.com>
2023-05-27 21:06:08 +0900

    [FEAT] HelloController

---
+++ hellobot/src/main/java/tobyspring/hellobot/HellobotApplication.java
@@ -0,0 +1,13 @@
+package tobyspring.hellobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellobotApplication.class, args);
+    }
+
+}
---
+++ hellobot/src/test/java/tobyspring/hellobot/HellobotApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.hellobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
