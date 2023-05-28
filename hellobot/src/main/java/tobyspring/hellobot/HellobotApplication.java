6b7c2206ba31f3c5b49d85cda2817f9bdb6b373a
kimtaedong <rlaxoehd4234@naver.com>
2023-05-28 19:08:55 +0900

    [FEAT] servlet server open

--- hellobot/src/main/java/tobyspring/hellobot/HellobotApplication.java
+++ hellobot/src/main/java/tobyspring/hellobot/HellobotApplication.java
@@ -1,13 +1,17 @@
 package tobyspring.hellobot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class HellobotApplication {
+    public static void main(String[] args){
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer =serverFactory.getWebServer();
+        webServer.start();
 
-    public static void main(String[] args) {
-        SpringApplication.run(HellobotApplication.class, args);
+
     }
 
 }
